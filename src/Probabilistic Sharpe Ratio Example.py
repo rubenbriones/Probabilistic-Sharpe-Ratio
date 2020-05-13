793e1248661c1d62a331f3740bcb09a920dc568c
Rubén Briones <rbriones@ets.es>
2020-05-13 12:31:34 +0200

    CLN: assume that returns are weekly

--- src/Probabilistic Sharpe Ratio Example.py
+++ src/Probabilistic Sharpe Ratio Example.py
@@ -1,7 +1,7 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: ipynb,py:light
+#     formats: notebooks//ipynb,src//py
 #     text_representation:
 #       extension: .py
 #       format_name: light
@@ -15,6 +15,12 @@
 
 # %load_ext autoreload
 # %autoreload 2
+
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
 
 # +
 import numpy as np
@@ -46,13 +52,13 @@ true_mean = dist_st1.stats('m').item()
 true_std  = np.sqrt(dist_st1.stats('v').item())
 # -
 
-# Generates random returns and check their moments statistic 
+# Generates random **weekly returns** and check their moments statistic 
 # (in a large sample the *skew* should be equal to `skew_returns`, **the other moments are not guaranteed to acommplish our initial conditions**)
 
-SIZE = 50
+SIZE = 52  # 1 year = 52 weeks
 
 # +
-SEED_ST1 = 44  # 5 39  44(13479)
+SEED_ST1 = 1131
 
 returns_st1 = dist_st1.rvs(size=SIZE, random_state=SEED_ST1)
 
@@ -65,7 +71,7 @@ print(moments_st1)
 sr_st1 = estimated_sharpe_ratio(returns_st1)
 print('SR st1 dist.:', sr_st1)
 
-sr_ann_st1 = ann_estimated_sharpe_ratio(returns_st1)
+sr_ann_st1 = ann_estimated_sharpe_ratio(returns_st1, periods=52)
 print('SR Annual. st1 dist.:', round(sr_ann_st1, 2))
 
 # ## Strategy 2 - *Simulate a normal distribution of returns with better `mean` and same `std` as the strategy 1*
@@ -73,15 +79,15 @@ print('SR Annual. st1 dist.:', round(sr_ann_st1, 2))
 # By definition the SR, in a big data sample, of this strategy (this returns distribution) must be greater than the SR of Strategy 1...but with a few data?
 
 # +
-EXTRA_EDGE = 0.0015
+EXTRA_EDGE = 0.0012
 
 dist_st2 = norm(loc=true_mean+EXTRA_EDGE, scale=true_std)
 # -
 
-# Generates random returns and check their moments statistic (in a large sample the *mean* and *std* should be equal to the `moments_st1`, and the *skew* should be 0 and *kurtosis* 3)
+# Generates random **weekly returns** and check their moments statistic (in a large sample the *mean* and *std* should be equal to the `moments_st1`, and the *skew* should be 0 and *kurtosis* 3)
 
 # +
-SEED_ST2 = 34430  # 1003 4223 4968 8039 11304(44)30481  34430
+SEED_ST2 = 9563
 
 returns_st2 = dist_st2.rvs(size=SIZE, random_state=SEED_ST2)
 
@@ -92,7 +98,7 @@ print(moments_st2)
 sr_st2 = estimated_sharpe_ratio(returns_st2)
 print('SR st2 dist.:', sr_st2)
 
-sr_ann_st2 = ann_estimated_sharpe_ratio(returns_st2)
+sr_ann_st2 = ann_estimated_sharpe_ratio(returns_st2, periods=52)
 print('SR Annual. st2 dist.:', round(sr_ann_st2, 2))
 
 # > Ooops, it seems that the Strategy 2 is worst than the Strategy 1... sure?
@@ -101,7 +107,7 @@ print('SR Annual. st2 dist.:', round(sr_ann_st2, 2))
 #
 # At this point, imagine that you want to invest your money in one of these two differenet investing strategies, so ***which one will you choose?***
 #
-# **Probably you will choose the first one**, because have an annualized sharpe ratio of 3.91 and the second strategy 3.74.
+# **Probably you will choose the first one**, because have an annualized sharpe ratio of 1.63 and the second strategy of 1.55.
 #
 # ### But, is this a good decision?
 #
@@ -116,7 +122,7 @@ psr_st2 = probabilistic_sharpe_ratio(returns_st2, sr_benchmark=SR_BENCHMARK)
 print('PSR st2:', psr_st2)
 
 # > As you can see the Probabilistic Sharpe Ratio (PSR) says that despite of the lower SR^ of the second strategy, this strategy 2 have more chances that in the future its returns will have a TRUE SR greater than 0.  
-# **We can say with a 95.66% confidence that the strategy 2 it's a winner strategy, but we "only" have a certainty of 92.89% that the strategy 1 can generate *alpha* too.**
+# **We can say with a 95.19% confidence that the strategy 2 it's a winner strategy, but we "only" have a certainty of 92.98% that the strategy 1 can generate *alpha* too.**
 
 # Plotting the returns distribution, we can confirm that strategy 1 returns have a big negative skewness, and this makes bigger the stdDev of its SR^ estimates, so we have less certainty on our estimation (lower PSR).
 
@@ -148,7 +154,7 @@ true_moments_st1 = moments(oos_returns_st1)
 print(true_moments_st1)
 # -
 
-true_sr_ann_st1 = ann_estimated_sharpe_ratio(oos_returns_st1)
+true_sr_ann_st1 = ann_estimated_sharpe_ratio(oos_returns_st1, periods=52)
 print('TRUE SR Annual. Strategy 1:', round(true_sr_ann_st1, 2))
 
 # +
@@ -158,32 +164,19 @@ true_moments_st2 = moments(oos_returns_st2)
 print(true_moments_st2)
 # -
 
-true_sr_ann_st2 = ann_estimated_sharpe_ratio(oos_returns_st2)
+true_sr_ann_st2 = ann_estimated_sharpe_ratio(oos_returns_st2, periods=52)
 print('TRUE SR Annual. Strategy 2:', round(true_sr_ann_st2, 2))
 
 # ## Conclusions
-# #### We can see that in the long term the Strategy 2 is much better than the Strategy 1. Its TRUE SR is around 9.46 and the TRUE SR of Strategy 1 is around 5.43. 
+# #### We can see that in the long term the Strategy 2 is much better than the Strategy 1. Its TRUE SR is around 3.85 and the TRUE SR of Strategy 1 is around 2.42. 
 # #### This is due to the `EXTRA_EDGE` we added tothe returns distributions of the Strategy 2. 
 # #### So, despite of the lower SR^ in the small sample, we were able to choose the best Strategy using the Probabilistic Sharpe Ratio (PSR). 
-# In 3 years our money will have grown x3 times more (aprox.) than if we had invested in the Strategy 1!
+# In 5 years our money will have grown x3 times more (aprox.) than if we had invested in the Strategy 1!
 
-pd.DataFrame({'Strategy 1': pd.Series(oos_returns_st1).add(1).cumprod().sub(1).iloc[:262*3],
-              'Strategy 2': pd.Series(oos_returns_st2).add(1).cumprod().sub(1).iloc[:262*3]}).plot()
+pd.DataFrame({'Strategy 1': pd.Series(oos_returns_st1).add(1).cumprod().sub(1).iloc[:52*5],
+              'Strategy 2': pd.Series(oos_returns_st2).add(1).cumprod().sub(1).iloc[:52*5]}).plot()
 
 # NOTE: In this notebook we have checked the impact of the skewness in the SR^ but the kurtosis also have an impact on the SR^ and its estimated error. But there is no way for generating random returns with a predefined kurtosis.
 # > Let me know in a issue if you now one way to generate random returns with a desired kurtosis, or with a skewness greater than [-0.99, 0.99].
-
-returns_st1
-
-# pd.Series.to_csv()
-pd.Series(returns_st1).to_csv('returns_st1.csv', index=False)
-
-ret= pd.read_csv('returns_st1.csv', header=None, squeeze=True)
-
-import quantstats as qs
-
-qs.stats.sharpe(ret, annualize=False)
-
-estimated_sharpe_ratio(ret)
 
 
