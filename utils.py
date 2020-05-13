d744bb5c8f473737b6fa7f9fa2751cf5455f4d6f
Rubén Briones <rbriones@ets.es>
2020-05-13 02:29:58 +0200

    ENH: add min_track_record_length calculation

--- utils.py
+++ utils.py
@@ -135,6 +135,63 @@ def probabilistic_sharpe_ratio(returns=None, sr_benchmark=0.0, *, sr=None, sr_st
     return psr
 
 
+def min_track_record_length(returns=None, sr_benchmark=0.0, prob=0.95, *, n=None, sr=None, sr_std=None):
+    """
+    Calculate the MIn Track Record Length (minTRL).
+
+    Parameters
+    ----------
+    returns: np.array, pd.Series, pd.DataFrame
+        If no `returns` are passed it is mandatory to pass a `sr` and `sr_std`.
+
+    sr_benchmark: float
+        Benchmark sharpe ratio expressed in the same frequency as the other parameters.
+        By default set to zero (comparing against no investment skill).
+
+    prob: float
+        Confidence level used for calculating the minTRL.
+        Between 0 and 1, by default=0.95
+
+    n: int
+        Number of returns samples used for calculating `sr` and `sr_std`.
+
+    sr: float, np.array, pd.Series, pd.DataFrame
+        Sharpe ratio expressed in the same frequency as the other parameters.
+
+    sr_std: float, np.array, pd.Series, pd.DataFrame
+        Standard deviation fo the Estimated sharpe ratio,
+        expressed in the same frequency as the other parameters.
+
+    Returns
+    -------
+    float, pd.Series
+
+    Notes
+    -----
+    minTRL = minimum of returns/samples needed (with same SR and SR_STD) to accomplish a PSR(SR*) > `prob`
+    PSR(SR*) = probability that SR^ > SR*
+    SR^ = sharpe ratio estimated with `returns`, or `sr`
+    SR* = `sr_benchmark`
+
+    https://papers.ssrn.com/sol3/papers.cfm?abstract_id=1821643
+    """
+    if n is None:
+        n = len(returns)
+    if sr is None:
+        sr = estimated_sharpe_ratio(returns)
+    if sr_std is None:
+        sr_std = estimated_sharpe_ratio_stdev(returns)
+
+    min_trl = 1 + (sr_std ** 2 * (n - 1)) * (scipy.stats.norm.ppf(prob) / (sr - sr_benchmark)) ** 2
+
+    if type(returns) == pd.DataFrame:
+        min_trl = pd.Series(min_trl, index=returns.columns)
+    elif type(min_trl) not in (float, np.float64):
+        min_trl = min_trl[0]
+
+    return min_trl
+
+
 def skew_to_alpha(skew):
     """
     Convert a skew to alpha parameter needed by scipy.stats.skewnorm(..).
